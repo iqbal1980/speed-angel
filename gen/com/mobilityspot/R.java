ccfcd9f997d916440b7d7ca075bfcfbe546686e1
iqbal.addou@gmail.com <iqbal.addou@gmail.com@2c1ae896-af93-5554-ffdb-e7b27c7610a6>
2012-10-12 20:35:10 +0000

    The flow is working fine now, just minor changes to workflow.

--- gen/com/mobilityspot/R.java
+++ gen/com/mobilityspot/R.java
@@ -21,6 +21,7 @@ public final class R {
         public static final int angel=0x7f020000;
         public static final int ic_launcher=0x7f020001;
         public static final int splash=0x7f020002;
+        public static final int thankyousplash=0x7f020003;
     }
     public static final class id {
         public static final int blocktext=0x7f080000;
@@ -39,6 +40,7 @@ public final class R {
         public static final int dummy=0x7f030001;
         public static final int main=0x7f030002;
         public static final int splash=0x7f030003;
+        public static final int thankyousplash=0x7f030004;
     }
     public static final class string {
         public static final int app_name=0x7f050001;
